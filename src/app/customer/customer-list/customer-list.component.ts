27515084fe1e2849b0945c76ca6ac5d6e2566f7c
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2023-10-09 13:47:48 +0700

    directive like a ngIf & ngFor, interpolation like {{data}}

--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -5,6 +5,7 @@ import { ICustomer } from 'src/app/shared/interfaces';
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.css'],
   //   template: `<p>tess</p>`,
 })
 export class CustomerListComponent implements OnInit {
@@ -14,5 +15,45 @@ export class CustomerListComponent implements OnInit {
   currencyCode: string = 'USD';
 
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {
+    //this is only for the test : is the component was render properly. The data will put on different way, also the customersOrderTotal.
+
+    this.filteredCustomers = [
+      {
+        id: 1,
+        name: 'john Doe',
+        city: 'Phoenix',
+        orderTotal: 9.99,
+        customerSince: new Date(2014, 7, 10),
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        city: 'Chandler',
+        orderTotal: 19.99,
+        customerSince: new Date(2017, 2, 22),
+      },
+      {
+        id: 3,
+        name: 'Michelle Thomas',
+        city: 'Seattle',
+        orderTotal: 99.99,
+        customerSince: new Date(2002, 10, 31),
+      },
+      {
+        id: 4,
+        name: 'Jim Thomas',
+        city: 'New York',
+        orderTotal: 599.99,
+        customerSince: new Date(2002, 10, 31),
+      },
+    ];
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((cust: ICustomer) => {
+      this.customersOrderTotal += cust.orderTotal || 0;
+    });
+  }
 }
