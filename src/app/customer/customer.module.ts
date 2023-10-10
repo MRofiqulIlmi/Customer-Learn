ff0cb8f3877caf2359578d7ecf592757cc063d78
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2023-10-10 14:54:51 +0700

    Pipe, can be manipulate for show the data by | symbol

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -5,10 +5,15 @@ import { CommonModule } from '@angular/common';
 import { CustomerComponent } from './customer.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { FilterTextBoxComponent } from './customer-list/filter-texbox.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [CustomerComponent, CustomerListComponent, FilterTextBoxComponent],
+  imports: [CommonModule, SharedModule],
+  declarations: [
+    CustomerComponent,
+    CustomerListComponent,
+    FilterTextBoxComponent,
+  ],
   //   providers: [CustomerComponent],
   exports: [CustomerComponent],
   //   bootstrap: [AppComponent],
