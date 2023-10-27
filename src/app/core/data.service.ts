4ac7895a6790684076235356473c9e8fff75062f
Muhammad Rofiqul Ilmi <muhammadrofiqulilmi@gmail.com>
2023-10-27 15:08:22 +0700

    add : calling the server using http client - Observable for async, http get, and catch error

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
+import { throwError, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 import { map, catchError } from 'rxjs/operators';
@@ -10,7 +10,35 @@ import { ICustomer, IOrder } from '../../app/shared/interfaces';
 export class DataService {
   baseUrl: string = 'assets/';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  //observable use for async process like a http request. than ICustomer[] is expected return from this function,
+  // and the pipe is for a task after process get success before provide to component, like filtering only the id is true. or do something is error happen.
+  getCustomers(): Observable<ICustomer[]> {
+    return this.http
+      .get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(catchError(this.handleError));
+  }
+
+  getCustomer(id: number): Observable<ICustomer[]> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json').pipe(
+      map((customers) => {
+        let customer = customers.filter((cust: ICustomer) => cust.id == id);
+        return customer;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getOrders(id: number): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + '/orders.json').pipe(
+      map((orders) => {
+        let custOrder = orders.filter((ord: IOrder) => ord.customerId == id);
+        return custOrder;
+      }),
+      catchError(this.handleError)
+    );
+  }
 
   private handleError(error: any) {
     console.error('server error:', error);
